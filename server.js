116ee75da9c5e6142a2f79da693c701f89ac5863
Seiji <v-seiji.villafranca@politemail.com>
2022-08-18 22:29:56 +0800

    add auth server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth')
 const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // <== Will be created later
 const middlewares = jsonServer.defaults();
@@ -6,5 +7,6 @@ const port = process.env.PORT || 3200; // <== You can change the port
 
 server.use(middlewares);
 server.use(router);
+server.use(auth)
 
 server.listen(port);
